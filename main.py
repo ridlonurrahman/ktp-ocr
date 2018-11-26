3518a2d617164c80073aa1bc9112c35e7112536a
Ridlo Nur Rahman <ridlonurrahman@users.noreply.github.com>
2018-11-27 02:38:14 +0800

    Edit image id variable name

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import cv2
 from vision_api_ocr import VisionAPIOCRLocationBased
 
 # import image
-driver_id = 541436371
-image = cv2.imread('../stage-one/data/ktps/{}.jpg'.format(driver_id))
+image_id = 541436371
+image = cv2.imread('../stage-one/data/ktps/{}.jpg'.format(image_id))
 
 # create api object
 api = VisionAPIOCRLocationBased(image)
